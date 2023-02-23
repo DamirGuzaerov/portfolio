b038b05d41ba0b7609eb527ab0eaefc5e9598f8b
DamirGuzaerov <guzaerov.damir49@gmail.com>
2023-02-24 02:27:28 +0500

    [add] migrations

--- web/models.py
+++ web/models.py
@@ -2,14 +2,13 @@ from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-
 # Create your models here.
 
 User = get_user_model()
 
 
 class Skill(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=128)
     level = models.PositiveIntegerField(default=10, validators=[MinValueValidator(0), MaxValueValidator(100)])
 
 
@@ -30,11 +29,7 @@ class Project(models.Model):
     description = models.CharField(max_length=512)
     link_to_source_code = models.CharField(max_length=512)
     link_to_production = models.CharField(max_length=512)
-    preview_img_url = models.CharField(max_length=512)
+    preview_img = models.ImageField(upload_to='project_previews/', null=False, blank=True)
     experience = models.ForeignKey(Experience, on_delete=models.CASCADE)
     skills = models.ManyToManyField(Skill)
-
-
-
-
 

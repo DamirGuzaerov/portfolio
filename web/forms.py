ebc0bc3bbbc7fe396920a675b890d0a2a8c4e5f0
DamirGuzaerov <guzaerov.damir49@gmail.com>
2023-03-06 23:31:55 +0500

    [add] first pages

---
+++ web/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+
+class RegistrationForm(forms.Form):
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput())
+    password_confirm = forms.CharField(widget=forms.PasswordInput())

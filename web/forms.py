db5c934e8e2956fd521bb1c54f6c55f6d11634ba
DamirGuzaerov <guzaerov.damir49@gmail.com>
2023-03-08 18:00:16 +0500

    [add] registration

--- web/forms.py
+++ web/forms.py
@@ -1,7 +1,18 @@
 from django import forms
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
-class RegistrationForm(forms.Form):
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput())
+class RegistrationForm(forms.ModelForm):
     password_confirm = forms.CharField(widget=forms.PasswordInput())
+
+    def clean(self):
+        cleaned_data = super().clean()
+        if cleaned_data['password'] != cleaned_data['password_confirm']:
+            self.add_error('password_confirm', 'Passwords are not the same')
+        return cleaned_data
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'password_confirm')
--- web/views.py
+++ web/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
 from web.forms import RegistrationForm
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 menu = ['Projects', 'Skills', 'Contacts']
 
@@ -11,10 +14,17 @@ def main_view(request):
 
 def registration_view(request):
     form = RegistrationForm()
-    return render(request, 'web/registration.html', {'form': form, 'menu': menu})
+
+    if request.method == 'POST':
+        form = RegistrationForm(data=request.POST)
+        if form.is_valid():
+            user = User(email=form.cleaned_data['email'],username=form.cleaned_data['username'])
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+    return render(request, 'web/registration.html',
+                  {'form': form, 'menu': menu})
 
 
 def projects_view(request):
     form = RegistrationForm()
     return render(request, 'web/projects.html', {'form': form, 'menu': menu})
-
